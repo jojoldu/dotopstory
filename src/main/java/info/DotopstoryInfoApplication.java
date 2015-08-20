28b5403c792571e26689cad4516ef0a00bbbbd50
jojoldu <jojoldu@gmail.com>
2015-08-21 01:13:19 +0900

    프로젝트 생성

---
+++ src/main/java/info/DotopstoryInfoApplication.java
@@ -0,0 +1,12 @@
+package info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotopstoryInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotopstoryInfoApplication.class, args);
+    }
+}
---
+++ src/test/java/info/DotopstoryInfoApplicationTests.java
@@ -0,0 +1,18 @@
+package info;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DotopstoryInfoApplication.class)
+@WebAppConfiguration
+public class DotopstoryInfoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
