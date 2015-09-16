61789086abc03bd8ea5f8d3222ecd0ce90cd4b0d
이동욱 <jojoldu@zuminternet.com>
2015-09-16 09:47:19 +0900

    lombok 적용중

--- src/main/java/dotop/controller/HeroController.java
+++ src/main/java/dotop/controller/HeroController.java
@@ -1,5 +1,8 @@
 package dotop.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HeroController {
 
 }
---
+++ src/main/java/dotop/domain/Status.java
@@ -0,0 +1,10 @@
+package dotop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="status")
+public class Status {
+
+}
